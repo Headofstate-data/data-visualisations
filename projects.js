8afe9644f2766b9216e36880e5d21826322f9f57
Headofstate-data <damilolaojikutu4@gmail.com>
2025-02-17 12:09:33 +0100

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,17 @@
+const projects = [
+    {
+        title: "Project One",
+        description: "This is my first project. It does amazing things!",
+        link: "https://github.com/yourusername/project-one"
+    },
+    {
+        title: "Project Two",
+        description: "A cool project that automates tasks.",
+        link: "https://github.com/yourusername/project-two"
+    },
+    {
+        title: "Project Three",
+        description: "An AI-powered web app.",
+        link: "https://github.com/yourusername/project-three"
+    }
+];
