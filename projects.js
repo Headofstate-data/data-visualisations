37958ebe2b7438e5f7ba8e4ed111c4b283ad5860
Headofstate-data <damilolaojikutu4@gmail.com>
2025-03-12 09:49:12 +0100

    Update projects.js

--- projects.js
+++ projects.js
@@ -5,13 +5,13 @@ const projects = [
         link: "https://github.com/Headofstate-data/Headofstate-data/compare/main...Headofstate-data-Machinelearning?expand=1"
     },
     {
-        title: "SEGMENTING AND CLASSIFYING THE BEST STRIKERSo",
-        description: "We take a deep dive into a dataset containing information on 500 strikers, aiming to uncover patterns, insights, and classifications that distinguish top-performing strikers from the rest.",
+        title: "SEGMENTING AND CLASSIFYING THE BEST STRIKERS",
+        description: "This project analyzes a dataset of 500 strikers to identify patterns, insights, and classifications that differentiate top-performing strikers from the rest..",
         link: "https://github.com/Headofstate-data/Headofstate-data/tree/main"
     },
     {
-        title: "Project Three",
-        description: "An AI-powered web app.",
+        title: "CREDIT CARD FRAUD DETECTION USING MACHINE LEARNING",
+        description: "implementation of a fraud detection model using Python, leveraging the Credit Card Fraud Detection dataset.",
         link: "https://github.com/yourusername/project-three"
     }
 ];
