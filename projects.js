069d92720bc1658a4b7ee19ce99f5b0b0cfcbbc8
Headofstate-data <damilolaojikutu4@gmail.com>
2025-03-12 09:30:12 +0100

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,13 +1,13 @@
 const projects = [
     {
-        title: "Machine Learning ",
+        title: "SENTIMENT ANALYSIS USING MACHINE LEARNING ",
         description: " classification models to conduct sentiment analysis on the given dataset",
         link: "https://github.com/Headofstate-data/Headofstate-data/compare/main...Headofstate-data-Machinelearning?expand=1"
     },
     {
-        title: "Project Two",
-        description: "A cool project that automates tasks.",
-        link: "https://github.com/yourusername/project-two"
+        title: "SEGMENTING AND CLASSIFYING THE BEST STRIKERSo",
+        description: "We take a deep dive into a dataset containing information on 500 strikers, aiming to uncover patterns, insights, and classifications that distinguish top-performing strikers from the rest.",
+        link: "https://github.com/Headofstate-data/Headofstate-data/tree/main"
     },
     {
         title: "Project Three",
